7ad37aca009ea2a73337ad03bb5e40ed4b724bd4
Angel <mendeza20057@gmail.com>
2026-02-20 08:12:44 -0600

    Se agrego controller de auth para el proyecto

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -1,1 +1,44 @@
-import bcryptjs from "bcryptjs";
+import bcrypt from "bcryptjs";
+import User from "../models/User.js";
+import { generateJWT } from "../helpers/generate-jwt.js";
+
+export const login = async (req, res) => {
+    const { correo, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({
+                msg: "The credentials are not correct - email"
+            });
+        }
+
+        if (!user.state) {
+            return res.status(400).json({
+                msg: "The credentials are not correct - state: false"
+            });
+        }
+
+        const validPassword = bcrypt.compareSync(password, user.password);
+        if (!validPassword) {
+            return res.status(400).json({
+                msg: "The credentials are not correct - password"
+            });
+        }
+
+        const token = await generateJWT(user.id);
+
+        res.status(200).json({
+            msg: 'Login OK!!!',
+            user,
+            token
+        });
+
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            msg: 'An error occurred while trying to log in'
+        });
+    }
+}
